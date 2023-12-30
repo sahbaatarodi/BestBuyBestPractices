5140eeae10455b36fcf1e21eec113a3e81a66dd0
sahba atarodi <sahba.atarodi@gmail.com>
2023-12-29 21:24:35 -0500

    done

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public DapperProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM Products;");
+        }
+    }
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -24,3 +24,18 @@ foreach (var department in departments)
 
 
 }
+
+var productRepository = new DapperProductRepository(conn);
+var products = productRepository.GetAllProducts();
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductId);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
+    Console.WriteLine();
+    Console.WriteLine();
+
+}
